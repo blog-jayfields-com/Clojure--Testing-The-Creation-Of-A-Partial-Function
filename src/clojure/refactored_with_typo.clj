9ddd6e55e182f317dea9ec2f0b577904a87f2bf6
Jay Fields <github@jayfields.com>
2013-05-12 09:59:44 -0400

    different typo

--- src/clojure/refactored_with_typo.clj
+++ src/clojure/refactored_with_typo.clj
@@ -6,9 +6,9 @@
   (-> bac (+ bac-increase) (>= 0.08)))
 
 (defn update-bac [x]
-  (swap! state assoc :legally-drunk?* (partial pure-legally-drunk? x)))
+  (swap! state assoc :legally-drunk?** (partial pure-legally-drunk? x)))
 
 (defn legally-drunk? [bac-increase]
-  (let [{:keys [legally-drunk?**]
-         :or {legally-drunk?** (partial pure-legally-drunk? 0)}} @state]
+  (let [{:keys [legally-drunk?*]
+         :or {legally-drunk?* (partial pure-legally-drunk? 0)}} @state]
     (legally-drunk?* bac-increase)))
